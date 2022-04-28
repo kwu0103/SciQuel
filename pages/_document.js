d91a07a9b2083b968c8636c25e6049dab2be72ae
Luke Lavan <luke.lavan01@gmail.com>
2022-04-27 22:58:52 -0400

    remove unused package and script

--- pages/_document.js
+++ pages/_document.js
@@ -1,20 +1,27 @@
-import { Html, Head, Main, NextScript } from 'next/Document'
+import { Html, Head, Main, NextScript } from "next/Document";
+import Script from "next";
 
 export default function Document() {
   return (
     <Html>
       <Head>
-      <link href='https://fonts.googleapis.com/css?family=Alegreya+Sans+SC&display=optional' rel='stylesheet' />
-      <link href='https://fonts.googleapis.com/css?family=Quicksand&display=optional' rel='stylesheet' />
-      <link href='https://fonts.googleapis.com/css?family=Source+Serif+Pro&display=optional' rel='stylesheet' />
-
-      <script src="https://kit.fontawesome.com/450b4b1117.js" crossOrigin="anonymous"></script>
-      
+        <link
+          href="https://fonts.googleapis.com/css?family=Alegreya+Sans+SC&display=optional"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css?family=Quicksand&display=optional"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css?family=Source+Serif+Pro&display=optional"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
